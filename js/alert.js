602c3b6e1caef97aa14d6f24d8575a783e72b5f0
githun122 <githun122@163.com>
2017-07-24 07:46:14 +0800

    aaaaaaa

---
+++ js/alert.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#tc").click(function () {
+        $("#box").css("display","block");
+    });
+    $("#sure").click(function () {
+        $("#box").css("display","none");
+    });
+});
